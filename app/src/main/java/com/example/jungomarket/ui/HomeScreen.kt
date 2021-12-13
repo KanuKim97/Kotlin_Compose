9ab3ebb95bd1236c3305a85370fe9cfd2327763e
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2021-12-14 00:15:46 +0900

    add FeaturedSection.kt
    add FeaturedSection.kt

--- app/src/main/java/com/example/jungomarket/ui/HomeScreen.kt
+++ app/src/main/java/com/example/jungomarket/ui/HomeScreen.kt
@@ -1,10 +1,13 @@
 package com.example.jungomarket.ui
 
+import androidx.compose.ui.graphics.Path
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ButtonColors
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -16,12 +19,19 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jungomarket.R
+import com.example.jungomarket.R.drawable
+import com.example.jungomarket.featuredSection
+import com.example.jungomarket.standardQuadFromTo
 
+@ExperimentalFoundationApi
 @Composable
 fun homeScreen() {
     Box(
@@ -32,6 +42,43 @@ fun homeScreen() {
         Column {
             greetingSection()
             chipSection(chips = listOf("Sweet Sleep", "Insomnia", "Depression"))
+            currentMeditation()
+            featureSection(
+                features = listOf(
+                    featuredSection(
+                        title = "Sleep meditation",
+                        R.drawable.ic_headphone,
+                        Color(0xffaeb4fd),
+                        Color(0xff9fa5fe),
+                        Color(0xff8f98fd)
+                    ),
+
+                    featuredSection(
+                        title = "Tips for sleeping",
+                        R.drawable.ic_videocam,
+                        Color(0xff54e1b6),
+                        Color(0xff36ddab),
+                        Color(0xff11d79b)
+                    ),
+
+                    featuredSection(
+                        title = "Night island",
+                        R.drawable.ic_headphone,
+                        Color(0xfff0bd28),
+                        Color(0xfff1c746),
+                        Color(0xfff4cf65)
+                    ),
+
+                    featuredSection(
+                        title = "Calming Sounds",
+                        R.drawable.ic_headphone,
+                        Color(0xfffdbda1),
+                        Color(0xfffcaf90),
+                        Color(0xfff9a27b)
+                    )
+
+                )
+            )
         }
 
     }
@@ -53,7 +100,7 @@ fun greetingSection(
         ) {
             Text(
                 text = "Good Morning!, $name",
-                style = MaterialTheme.typography.h3,
+                style = MaterialTheme.typography.h5,
                 color = Color.White
             )
 
@@ -64,7 +111,7 @@ fun greetingSection(
             )
         }
         Icon(
-            painter = painterResource(id = R.drawable.ic_search),
+            painter = painterResource(id = drawable.ic_search),
             contentDescription = "Search",
             modifier = Modifier.size(24.dp),
             tint = Color.White
@@ -106,11 +153,176 @@ fun chipSection(
 
 @Composable
 fun currentMeditation(
-    color: Color
+    color: Color = Color(0xfffc879a)
 ){
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .padding(15.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(color)
+            .padding(horizontal = 15.dp, vertical = 20.dp)
+            .fillMaxWidth()
+    ) {
+        Column {
+
+            Text(
+                text = "Daily Thought",
+                style = MaterialTheme.typography.h5,
+                color = Color.White
+            )
+            Text(
+                text = "Meditation, 3-10min",
+                style = MaterialTheme.typography.body1,
+                color = Color.White
+            )
+
+        }
+
+        Box(
+          contentAlignment = Alignment.Center,
+          modifier = Modifier
+              .size(40.dp)
+              .clip(CircleShape)
+              .background(Color(0xff505cf3))
+              .padding(10.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = drawable.ic_play),
+                contentDescription = "Play",
+                tint = Color.White,
+                modifier = Modifier.size(16.dp)
+            )
+        }
+
+    }
+}
+
+@ExperimentalFoundationApi
+@Composable
+fun featureSection(features: List<featuredSection>){
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = "Features",
+            style = MaterialTheme.typography.h5,
+            color = Color.White,
+            modifier = Modifier.padding(15.dp)
+        )
+        LazyVerticalGrid(
+            cells = GridCells.Fixed(2),
+            contentPadding = PaddingValues(start = 7.5.dp, end = 7.5.dp, bottom = 100.dp),
+            modifier = Modifier.fillMaxHeight()
+        ) {
+            items(features.size) {
+                featureItem(feature = features[it])
+            }
+        }
+    }
+}
+
+@Composable
+fun featureItem(
+    feature: featuredSection
+){
+    BoxWithConstraints(
+        modifier = Modifier
+            .padding(7.5.dp)
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(10.dp))
+            .background(feature.darkColor)
+    ) {
+        val height = constraints.maxHeight
+        val width = constraints.maxWidth
+
+        val mediumColoredPoint1 = Offset(0f, height * 0.3f)
+        val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
+        val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
+        val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
+        val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
+
+        val mediumColoredPath = Path().apply {
+            moveTo(mediumColoredPoint1.x, mediumColoredPoint1.y)
+            standardQuadFromTo(mediumColoredPoint1, mediumColoredPoint2)
+            standardQuadFromTo(mediumColoredPoint2, mediumColoredPoint3)
+            standardQuadFromTo(mediumColoredPoint3, mediumColoredPoint4)
+            standardQuadFromTo(mediumColoredPoint4, mediumColoredPoint5)
+            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+            lineTo(-100f, height.toFloat() + 100f)
+            close()
+        }
+
+        val lightPoint1 = Offset(0f, height * 0.3f)
+        val lightPoint2 = Offset(width * 0.1f, height * 0.4f)
+        val lightPoint3 = Offset(width * 0.3f, height * 0.35f)
+        val lightPoint4 = Offset(width * 0.65f, height.toFloat())
+        val lightPoint5 = Offset(width * 1.4f, -height.toFloat() / 3f)
+
+        val lightColoredPath = Path().apply {
+            moveTo(lightPoint1.x, lightPoint1.y)
+            standardQuadFromTo(lightPoint1, lightPoint2)
+            standardQuadFromTo(lightPoint2, lightPoint3)
+            standardQuadFromTo(lightPoint3, lightPoint4)
+            standardQuadFromTo(lightPoint4, lightPoint5)
+            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+            lineTo(-100f, height.toFloat() + 100f)
+            close()
+        }
+        Canvas(
+            modifier = Modifier
+            .fillMaxSize()
+        ) {
+            drawPath(
+                path = mediumColoredPath,
+                color = feature.mediumColor
+            )
+            drawPath(
+                path = lightColoredPath,
+                color = feature.lightColor
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(15.dp)
+        ) {
+            Text(
+                text = feature.title,
+                style = MaterialTheme.typography.h6,
+                lineHeight = 26.sp,
+                modifier = Modifier.align(Alignment.TopStart)
+            )
+            Icon(
+                painter = painterResource(id = feature.iconId),
+                contentDescription = feature.title,
+                tint = Color.White,
+                modifier = Modifier.align(Alignment.BottomStart)
+            )
+            Text(
+                text = "Start",
+                color = Color.White,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .clickable {
+
+                    }
+                    .align(Alignment.BottomEnd)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(Color(0xff505cf3))
+                    .padding(vertical = 6.dp, horizontal = 15.dp)
+            )
+        }
+
+    }
+}
+
+@Composable
+fun bottomMenu(){
 
 }
 
+@ExperimentalFoundationApi
 @Preview
 @Composable
 fun preview(){
--- app/src/main/java/com/example/jungomarket/ui/theme/Type.kt
+++ app/src/main/java/com/example/jungomarket/ui/theme/Type.kt
@@ -2,6 +2,7 @@ package com.example.jungomarket.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
@@ -12,17 +13,18 @@ val Typography = Typography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
     ),
-    caption = TextStyle(
+
+    h1 = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
+    ),
+
+    h2 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
     )
-    */
+
 )
